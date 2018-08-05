f0b8995fddf6c6deada247f130f28c5cee94c996
Terence Jeong <terence.jeong@gmail.com>
2018-08-05 23:03:00 +1000

    some dumb mongo stuff

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/s5-appraisals");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/s5-appraisals");
 
 module.exports = mongoose;
