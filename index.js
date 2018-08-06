e55ba7c77ba10dd8522bd41de9f26414a72437bf
Terence Jeong <terence.jeong@gmail.com>
2018-08-06 19:06:27 +1000

    added cors

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 // lets us send JSON to the server. Let us parse incoming requests to JSON
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 //routes
 router(app);
